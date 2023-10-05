46ce532cac119d17a71d6d48fdaee122052e2d76
rasta-masta <143915828+rasta-masta@users.noreply.github.com>
2023-10-05 17:58:59 +0700

    empty-vite+cakra_ui

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Box, Heading, Button } from '@chakra-ui/react'
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <Box bg={'red'}>
+    <Heading>Welcome</Heading>
+    <Button colorScheme='lightblue'>ADD</Button>
+   </Box>
+  )
+}
+
+export default App
